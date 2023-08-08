74396457f7703977675afd581b4298455cc75993
Merlin Kim <merlinkim@Merlinui-MacBookPro.local>
2023-08-09 01:57:56 +0900

    create function

--- src/App.js
+++ src/App.js
@@ -42,30 +42,58 @@ function Article(props){
         <h2>{props.title}</h2>
         {props.body}
       </article>
-    )
+  )
+}
+
+function Create(props){
+  return(
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title,body);
+      }}>
+        <p><input type='text' name='title' placeholder='title'/></p>
+        <p><textarea name='body' placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  )
 }
 
 function App() {
   const [mode,setMode]=useState('WELCOME');
+
   const [id,setId]=useState(null);
-  const topics=[
+  const [nextId,setNextId]=useState(4);
+
+  const [topics,setTopics] = useState([
       {id:1, title:'html',body:'html is ....'},
       {id:2, title:'css',body:'css is ....'},
       {id:3, title:'js',body:'js is ....'}
-    ]
-
+    ]);
   let content = null;
+  
   if (mode === 'WELCOME'){
     content=<Article title="Welcome" body='Hello WEB'></Article>
   }else if (mode === 'read'){
     let title,body = null;
     for(let i=0; i<topics.length; i++){
-      if(topics[i].id==id){
+      if(topics[i].id === id){
         title=topics[i].title;
         body=topics[i].body;
       }
     }
     content=<Article title={title} body={body}></Article>
+  }else if (mode === 'create'){
+    content=<Create onCreate={(_title,_body)=>{
+      const newTopic = {id:nextId,title:_title,body:_body}
+      const newTopics=[...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+  }}></Create>
   }
   return (
     <div>
@@ -79,6 +107,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href='/ceate' onClick={event=>{
+        event.preventDefault();
+        setMode('create');
+      }}>create</a>
     </div>
   );
 }
