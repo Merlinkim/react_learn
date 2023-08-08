e8b23e76abec629b6a401d995c454730081cfd8b
Merlin Kim <merlinkim@Merlinui-MacBookPro.local>
2023-08-08 23:00:54 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function Header(props){
   console.log('props',props,props.title);
@@ -21,7 +22,7 @@ function Nav(props){
       <li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
       </li>
       ) 
@@ -45,22 +46,39 @@ function Article(props){
 }
 
 function App() {
+  const [mode,setMode]=useState('WELCOME');
+  const [id,setId]=useState(null);
   const topics=[
       {id:1, title:'html',body:'html is ....'},
       {id:2, title:'css',body:'css is ....'},
       {id:3, title:'js',body:'js is ....'}
     ]
+
+  let content = null;
+  if (mode === 'WELCOME'){
+    content=<Article title="Welcome" body='Hello WEB'></Article>
+  }else if (mode === 'read'){
+    let title,body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id==id){
+        title=topics[i].title;
+        body=topics[i].body;
+      }
+    }
+    content=<Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title='WEB' onChangeMode={
         ()=>{
-          alert('Header');
+          setMode('WELCOME');
         }
       }></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('read');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body='Hello WEB'></Article>
+      {content}
     </div>
   );
 }
